0e8965e661ed721cbfba38aa844e15cb20653028
TabithaSusan <95419252+TabithaSusan@users.noreply.github.com>
2022-01-11 11:48:31 +0100

    Again new Project Files
    Due to several mistakes at the beginning, new project is easier to make up than fixing several mistakes due to false connections between files.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
-#include "loginscreen.h"
+#include "mainwindow.h"
 
 #include <QApplication>
-#include <QtSql/QSqlDatabase>
-#include <QtSql/QSqlQuery>
-#include <QDebug>
 
 int main(int argc, char *argv[])
 {
-
     QApplication a(argc, argv);
-    LoginScreen w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
