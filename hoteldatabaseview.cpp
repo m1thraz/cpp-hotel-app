0b6de0612b8b19d06777d0d340cd453b8dadfa8a
TahiraAiedail <59653267+TahiraAiedail@users.noreply.github.com>
2022-01-21 14:28:26 +0100

    Hoteldatenbank Aktualisieren fertig
    Hoteldatensätze können mit einem Klick auf den Button Aktualisieren modifiziert werden.
    !GUI Fenster fehlen noch. Zur Zeit nur Konsolenausgabe

--- hoteldatabaseview.cpp
+++ hoteldatabaseview.cpp
@@ -38,13 +38,15 @@ void hotelDatabaseView::on_aktualisierenButton_clicked() {
             int zimmernummer = std::stoi(tempZimmernummer.toStdString());
             //Überprüft, ob es das Zimmer mit der Zimmernummer X bereits gibt
             QSqlQuery query;
-            query.prepare("SELECT 1 FROM Zimmerbestand WHERE Zimmernummer = :zimmerbestand_zimmernummer;");
+            query.prepare("SELECT BestandID FROM Zimmerbestand WHERE Zimmernummer = :zimmerbestand_zimmernummer;");
             query.bindValue(":zimmerbestand_zimmernummer", zimmernummer);
             bool queryStatus = query.exec();
             qDebug() << "Abfrage erfolgreich: " << queryStatus;
-
+            bool exists = false;
             //Wird nur ausgeführt, wenn es das Zimmer tatsächlich gibt, sonst Fehlermeldung
             while(query.next()) {
+                exists = true;
+                int bestandID = std::stoi(query.value("BestandID").toString().toStdString());
                 // QCheckBox Felder werden ausgewertet
                 bool doppelbett = ui->checkBoxModDoppelbett->isChecked();
                 bool einzelbett = ui->checkBoxModEinzelbett->isChecked();
@@ -56,36 +58,43 @@ void hotelDatabaseView::on_aktualisierenButton_clicked() {
                 if(doppelbett && !einzelbett) {
                     veraenderung = true;
                     query.prepare("UPDATE Zimmerbestand SET ZimmerID = 2 WHERE Zimmernummer = :zimmerbestand_zimmernummer;");
-                   // query.bindValue(":zimmerbestand_zimmernummer", zimmernummer);
+                    query.bindValue(":zimmerbestand_zimmernummer", zimmernummer);
                     queryStatus = query.exec();
                     qDebug() << "Update Doppelbett erfolgreich: " << queryStatus;
                 }else if(einzelbett && !doppelbett) {
                     veraenderung = true;
-                    query.prepare("UPDATE Zimmerzusatz SET ZimmerID = 1 WHERE Zimmernummer = :zimmerbestand_zimmernummer;");
+                    query.prepare("UPDATE Zimmerbestand SET ZimmerID = 1 WHERE Zimmernummer = :zimmerbestand_zimmernummer;");
+                    query.bindValue(":zimmerbestand_zimmernummer", zimmernummer);
                     queryStatus = query.exec();
                     qDebug() << "Update Einzelbett erfolgreich: " << queryStatus;
                 }
                 else if (doppelbett && einzelbett){
+                    veraenderung = true;
                     std::cout << "Bett kann nicht Einzelbett und Doppelbett gleichzeitig sein!" << std::endl;
                 }
-
                 if(aussicht) {
                     veraenderung = true;
-                    query.prepare("UPDATE BestandZusatzliste SET ZimmerzusatzID = 1 WHERE Zimmernummer = :zimmerbestand_zimmernummer;");
+                    query.prepare("INSERT OR IGNORE INTO BestandZusatzliste (BestandID, ZimmerzusatzID)"
+                                  "VALUES (:zimmerbestand_bestandID, 1);");
+                    query.bindValue(":zimmerbestand_bestandID", bestandID);
                     queryStatus = query.exec();
                     qDebug() << "Update Aussicht erfolgreich: " << queryStatus;
                 }
 
                 if(fahrstuhl) {
                     veraenderung = true;
-                    query.prepare("UPDATE BestandZusatzliste SET ZimmerzusatzID = 2 WHERE Zimmernummer = :zimmerbestand_zimmernummer;");
+                    query.prepare("INSERT OR IGNORE INTO BestandZusatzliste (BestandID, ZimmerzusatzID)"
+                                  "VALUES (:zimmerbestand_bestandID, 2);");
+                    query.bindValue(":zimmerbestand_bestandID", bestandID);
                     queryStatus = query.exec();
                     qDebug() << "Update Fahrstuhl erfolgreich: " << queryStatus;
                 }
 
                 if(sofa) {
                     veraenderung = true;
-                    query.prepare("UPDATE BestandZusatzliste SET ZimmerzusatzID = 3 WHERE Zimmernummer = :zimmerbestand_zimmernummer;");
+                    query.prepare("INSERT OR IGNORE INTO BestandZusatzliste (BestandID, ZimmerzusatzID)"
+                                  "VALUES (:zimmerbestand_bestandID, 3);");
+                    query.bindValue(":zimmerbestand_bestandID", bestandID);
                     queryStatus = query.exec();
                     qDebug() << "Update Sofa erfolgreich: " << queryStatus;
                 }
@@ -94,18 +103,57 @@ void hotelDatabaseView::on_aktualisierenButton_clicked() {
                     std::cout << "Bitte geben Sie neben der Zimmernummer die Veränderungswünsche an!" << std::endl;
                 }
             }
+            if(!exists) {
+                std::cout << "Zimmer existiert nicht!" << std::endl;
+            }
         }
+    }
+
     // Entweder nur ZimmerID oder Preis angegeben - Führt zu Error
-    }else if((tempZimmerID.isEmpty() && !tempPreis.isEmpty()) ||
+    if((tempZimmerID.isEmpty() && !tempPreis.isEmpty()) ||
              tempPreis.isEmpty() && !tempZimmerID.isEmpty()) {
         /*
          * GUI ERROR MESSAGE - Für das Anpassen eines Zimmerpreises, muss eine ZimmerID eingegeben werden.
          * Das Anpassen einer ZimmerID ist nicht möglich!
          */
         std::cout << "test" << std::endl;
+
+    // ZimmerID und Preis angegeben
     }else if(!tempZimmerID.isEmpty() && !tempPreis.isEmpty()) {
-         std::cout << "Preisanpassung" << std::endl;
+        QRegularExpression re("[0-9]+");
+        QRegularExpressionMatch matchZimmerID = re.match(tempZimmerID);
+        QRegularExpressionMatch matchPreis = re.match(tempPreis);
+
+        if(!matchZimmerID.hasMatch() || !matchPreis.hasMatch()) {
+            //GUI ERROR MESSAGE - Ungültiges Format. nur zahlen
+            std::cout << "Ungültiges Format" << std::endl;
+        } else {
+            std::cout << "Preisanpassung" << std::endl;
+            int zimmerID = std::stoi(tempZimmerID.toStdString());
+            int preis = std::stoi(tempPreis.toStdString());
+            //Überprüft, ob es die ZimmerID bereits gibt
+            QSqlQuery query;
+            query.prepare("SELECT 1 FROM Zimmer WHERE ZimmerID = :zimmer_zimmerID;");
+            query.bindValue(":zimmer_zimmerID", zimmerID);
+            bool queryStatus = query.exec();
+            qDebug() << "Abfrage erfolgreich: " << queryStatus;
+            bool exists = false;
+
+            //Wird nur ausgeführt, wenn es die ZimmerID tatsächlich gibt, sonst Fehlermeldung
+            while(query.next()) {
+                exists = true;
+                query.prepare("UPDATE Zimmer SET Zimmerkosten = :zimmer_preis WHERE ZimmerID = :zimmer_zimmerID;");
+                query.bindValue(":zimmer_preis", preis);
+                query.bindValue(":zimmer_zimmerID", zimmerID);
+                queryStatus = query.exec();
+                qDebug() << "Update Preis erfolgreich: " << queryStatus;
+            }
+            if(!exists) {
+                std::cout << "ZimmerID existiert nicht!" << std::endl;
+            }
+        }
     }
+    // Alle Eingabefelder (nicht Checkbox) sind leer
     else {
         std::cout << "nix ausgewählt" << std::endl;
     }
