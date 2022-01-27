8d41e107bf175a10a46ebdc40b1bf0b228ade90a
m1thraz <49205127+m1thraz@users.noreply.github.com>
2022-01-27 20:02:03 +0100

    rabatte hinzugefügt
    slider für rabaatte in der rechnungerstellung

--- invoicecreation.cpp
+++ invoicecreation.cpp
@@ -12,3 +12,30 @@ invoiceCreation::~invoiceCreation()
 {
     delete ui;
 }
+
+
+
+
+
+void invoiceCreation::on_rabattSlider_sliderMoved(int position)
+{
+ui->rabattBar->setValue(position);
+}
+
+void invoiceCreation::on_rabattAnRadio_toggled()
+{
+  ui->rabattSlider->setEnabled(true);
+  ui->rabattSlider->show();
+
+}
+
+void invoiceCreation::on_rabattAusRadio_toggled()
+{
+
+    ui->rabattSlider->setDisabled(true);
+    ui->rabattBar->setValue(0);
+    ui->rabattSlider->setValue(0);
+
+}
+
+
--- invoicecreation.h
+++ invoicecreation.h
@@ -4,19 +4,29 @@
 #include <QDialog>
 
 namespace Ui {
-class invoiceCreation;
+  class invoiceCreation;
 }
 
 class invoiceCreation : public QDialog
 {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    explicit invoiceCreation(QWidget *parent = nullptr);
-    ~invoiceCreation();
+  explicit invoiceCreation(QWidget *parent = nullptr);
+  ~invoiceCreation();
+
+private slots:
+
+  void on_rabattAnRadio_toggled();
+
+  void on_rabattAusRadio_toggled();
+
+  void on_rabattSlider_sliderMoved(int position);
+
+  void on_rabattAusRadio_clicked();
 
 private:
-    Ui::invoiceCreation *ui;
+  Ui::invoiceCreation *ui;
 };
 
 #endif // INVOICECREATION_H
